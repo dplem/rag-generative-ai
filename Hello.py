1a3087837f34693a125a32888b733f397ae5dcee
Jesse Henson <henson.jhenson.jesse@gmail.com>
2024-02-17 21:58:48 +0000

    Updated to show preview, change preview on url

--- Hello.py
+++ Hello.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import bs4
 from langchain import hub
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import WebBaseLoader
@@ -8,7 +7,7 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain.callbacks import get_openai_callback
-import getpass
+from linkpreview import link_preview, LinkGrabber
 import os
 
 # for compatibility with chromaDB versioning
@@ -25,11 +24,12 @@ dotenv.load_dotenv()
 
 st.set_page_config(
     page_title="Web QA",
-    page_icon="🌐",
+    page_icon="🕸️",
+    layout="wide"
 )
 
 
-def generate_response(url, question, openai_api_key):
+def generate_response(url, question):
     # Load, chunk and index the contents of the blog.
     loader = WebBaseLoader(
         web_paths=(url,),
@@ -38,12 +38,12 @@ def generate_response(url, question, openai_api_key):
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
     splits = text_splitter.split_documents(docs)
     vectorstore = Chroma.from_documents(documents=splits, 
-                                        embedding=OpenAIEmbeddings(openai_api_type=openai_api_key))
+                                        embedding=OpenAIEmbeddings(openai_api_key=openai_api_key))
 
     # Retrieve and generate using the relevant snippets of the blog.
     retriever = vectorstore.as_retriever()
     prompt = hub.pull("rlm/rag-prompt")
-    llm = ChatOpenAI(model_name="gpt-3.5-turbo", openai_api_key=openai_api_key, temperature=0)
+    llm = ChatOpenAI(model_name="gpt-3.5-turbo", openai_api_key=st.session_state.openai_api_key, temperature=0)
 
 
     def format_docs(docs):
@@ -63,45 +63,39 @@ def generate_response(url, question, openai_api_key):
 example_url = 'https://medium.com/@jesse.henson/get-answers-directly-from-documents-using-chatgpt-f02e83592caf'
 example_question = 'Summarize this article'
 
-openai_api_key = st.sidebar.text_input('OpenAI API Key')
-
-with st.form('my_form'):
-  url = st.text_area('Enter URL:', example_url)
-  question = st.text_area('Enter Question:', example_question)
-  submitted = st.form_submit_button('Submit')
-  if not openai_api_key.startswith('sk-'):
-    st.warning('Please enter your OpenAI API key!', icon='⚠')
-  if submitted and openai_api_key.startswith('sk-'):
-    with get_openai_callback() as cb:
-        generate_response(url, question, openai_api_key)
-        st.info(cb)
+with st.sidebar.form("API Key"):
+    openai_api_key = st.text_input('OpenAI API Key')
+    submitted = st.form_submit_button('Add Key')
+    if submitted:
+        if openai_api_key.startswith('sk-'):
+            st.session_state.openai_api_key = openai_api_key
+        else: 
+            st.sidebar.warning('Enter a different Key', icon='⚠')
+st.title("🕸️ Web QA")
+st.session_state.url = st.text_input('Enter URL:', example_url)
 
 
-
-
-# st.write(response)
+col1, col2 = st.columns([1,1], gap="large")
 
 
 
-# st.write("# Welcome to Streamlit! 👋")
+with col1:
+    with st.form('my_form'):
+        question = st.text_area('Enter Question:', example_question)
+        no_api_key = 'openai_api_key' not in st.session_state
+        submitted = st.form_submit_button('Submit', disabled=no_api_key)
+        if no_api_key:
+            st.warning('Please enter your OpenAI API key!', icon='⚠')            
+        if submitted and not no_api_key:
+            with get_openai_callback() as cb:
+                generate_response(st.session_state.url, question)
+                st.info(cb)
 
-# st.sidebar.success("Select a demo above.")
-
-# st.markdown(
-#     """
-#     Streamlit is an open-source app framework built specifically for
-#     Machine Learning and Data Science projects.
-#     **👈 Select a demo from the sidebar** to see some examples
-#     of what Streamlit can do!
-#     ### Want to learn more?
-#     - Check out [streamlit.io](https://streamlit.io)
-#     - Jump into our [documentation](https://docs.streamlit.io)
-#     - Ask a question in our [community
-#       forums](https://discuss.streamlit.io)
-#     ### See more complex demos
-#     - Use a neural net to [analyze the Udacity Self-driving Car Image
-#       Dataset](https://github.com/streamlit/demo-self-driving)
-#     - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-# """
-# )
-
+with col2:
+    if 'url' in st.session_state:
+        with st.expander("Preview",True):
+            preview = link_preview(st.session_state.url)
+            preview_image = preview.absolute_image
+            st.title(preview.title)
+            st.subheader(preview.description)
+            st.image(preview.image)
